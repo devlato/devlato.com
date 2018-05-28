6f5b05e3071f5fd929a125216957846a415bffb5
Denis Tokarev <github@devlato.com>
2018-05-28 16:46:37 +0200

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-git ls-files | xargs zip ./.build/build.zip unzip
-sshpass -p "$SSH_PASS" scp -rf ./.build/build.zip $SSH_USER@$SSH_HOST:/tmp/build.zip
-sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST unzip /tmp/build.zip -d $SERVER_ROOT
-rm -rf ./build/build.zip
+export SSHPASS=$SSH_PASS
+export LOCAL_ARCHIVE=./.build/release.zip
+export REMOTE_ARCHIVE=/tmp/release.zip
+
+echo Creating the archive...
+git ls-files | xargs zip $LOCAL_ARCHIVE
+
+echo Copying the archive to the remote host...
+sshpass -e scp -vr $LOCAL_ARCHIVE $SSH_USER@$SSH_HOST:$REMOTE_ARCHIVE
+
+echo Extracting the files from the archive...
+sshpass -e ssh SSH_USER@$SSH_HOST unzip $REMOTE_ARCHIVE -d $SERVER_ROOT
+
+echo Removing the remove archive...
+sshpass -e ssh SSH_USER@$SSH_HOST rm -rf $REMOTE_ARCHIVE
+
+echo Removing the local archive...
+rm -rf $LOCAL_ARCHIVE
+
+echo Done.
