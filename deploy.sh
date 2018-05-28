c58fa32f4cbcbad9d5d6cc4a3f3fe78a809f457b
Denis Tokarev <github@devlato.com>
2018-05-28 17:30:36 +0200

    Add version

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
 export SSHPASS=$SSH_PASS
-export LOCAL_ARCHIVE=./.build/release.zip
+export BUILD_DIR=./.build
+export VERSION_FILE=$BUILD_DIR/version.txt
+export LOCAL_ARCHIVE=$BUILD_DIR/release.zip
 export REMOTE_ARCHIVE=/tmp/release.zip
 
+export HEAD_COMMIT=$TRAVIS_COMMIT
+
+echo Creating the build directory...
+mkdir -p $BUILD_DIR
+
+echo Copying files to the build directory...
+git ls-files | xargs -IFILE bash -c 'export file=FILE; mkdir -p build/$( dirname $file ); cp -R $file build/$file'
+
+echo Generating the local version file...
+echo $HEAD_COMMIT > $VERSION_FILE
+
 echo Creating the archive...
-git ls-files | xargs zip $LOCAL_ARCHIVE
+zip -r $LOCAL_ARCHIVE $BUILD_DIR
 
 echo Copying the archive to the remote host...
 sshpass -e scp -vr $LOCAL_ARCHIVE $SSH_USER@$SSH_HOST:$REMOTE_ARCHIVE
@@ -16,7 +29,7 @@ sshpass -e ssh SSH_USER@$SSH_HOST unzip $REMOTE_ARCHIVE -d $SERVER_ROOT
 echo Removing the remove archive...
 sshpass -e ssh SSH_USER@$SSH_HOST rm -rf $REMOTE_ARCHIVE
 
-echo Removing the local archive...
-rm -rf $LOCAL_ARCHIVE
+echo Removing the build directory...
+rm -rf $BUILD_DIR
 
 echo Done.
